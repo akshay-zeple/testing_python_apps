d6302807a67fc65f49416eced9bfc351480350f5
akshay-zeple <akshay.zeple@gmail.com>
2021-01-13 12:45:22 +0530

    Added class Employee

--- git_demo_project/helloworld.py
+++ git_demo_project/helloworld.py
@@ -1,1 +1,19 @@
 print("Hello World!!")
+
+
+class Employee:
+    def __init__(self, name, pay):
+        self.name = name
+        self.pay = pay
+
+
+    @classmethod
+    def global_hike(cls, emp):
+        return cls(emp.name, emp.pay + 1000)
+
+
+emp = Employee("Akshay", 5000)
+emp_new = Employee.global_hike(emp)
+
+print(emp.pay)
+print(emp_new.pay)
